c5acc2b35307e4a62ca5b81a9fd17259dcdf0708
nohseulgi <135301920+nohseulgi@users.noreply.github.com>
2023-06-09 00:52:22 +0900

    Delete blog directory

--- blog/posts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Images(models.Model):
-    file = models.FileField(blank = True, upload_to='images/')
--- blog/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'posts'
-
-urlpatterns = [
-    path('index', views.index),
-    path('image1', views.image1),
-]
